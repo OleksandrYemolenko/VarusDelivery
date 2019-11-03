fca49e3f44f33e3723e9a92ffc5c92387da309a2
nXXL <saaho.rmolenko@gmail.com>
2019-11-03 18:10:38 +0200

    Initial commit

---
+++ app/src/main/java/KNU_kitkat/varusdelivery/ui/slideshow/SlideshowViewModel.java
@@ -0,0 +1,19 @@
+package KNU_kitkat.varusdelivery.ui.slideshow;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SlideshowViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public SlideshowViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is slideshow fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
