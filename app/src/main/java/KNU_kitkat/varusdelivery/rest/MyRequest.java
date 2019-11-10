370d722f6f3ac450d55f68c5815909b6b27277fe
Oleg Fomenko <oleg.fomenko2002@gmail.com>
2019-11-10 19:49:50 +0300

    url constants adding

--- app/src/main/java/KNU_kitkat/varusdelivery/rest/MyRequest.java
+++ app/src/main/java/KNU_kitkat/varusdelivery/rest/MyRequest.java
@@ -7,6 +7,18 @@ public class MyRequest {
     private String url;
     private JSONObject parms;
 
+    public static final String COMMON = "https://varus-delivery.herokuapp.com/";
+    public static final String GET_CATEGORIES = "getAllCategories";
+    public static final String GET_PRODUCTS_BY_CATEGORY = "getProductsByCategory";
+    public static final String ADD_ORDER = "addOrder";
+    public static final String GET_USER_ORDERS = "getOrdersByUser";
+    public static final String GET_USER_DELIVRIES = "getDeliveriesByUser";
+    public static final String SET_ORDER_STATUS = "setOrderStatus";
+    public static final String SET_DELIVERY_STATUS = "setDeliveryStatus";
+    public static final String SET_ORDER_PAYMENT_METHOD = "setOrderPayment";
+    public static final String SET_DELIVERY_PAYMENT_METHOD = "setDeliveryPayment";
+    public static final String SET_DELIVERY_PAYMENT_STATUS = "setDeliveryPaymentStatus";
+
     public MyRequest() {}
 
     public MyRequest(JSONObject data, String url, JSONObject parms) {
